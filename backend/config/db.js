1b261c5abf6b0a130091ecdb509afe10a4d1dff6
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2022-04-24 21:37:31 +0300

    data seeder scripts and models

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,15 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      // useCreateIndex: true,
+    });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${error.message}`.red.underline.bold);
+    console.log(`Error: ${error.message}`);
     process.exit(1);
   }
 };
